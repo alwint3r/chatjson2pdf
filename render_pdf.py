2d067009f8faa81b129d3af78c86a14c28639fcf
Alwin Arrasyid <alwin.ridd@gmail.com>
2024-01-09 15:19:14 +0700

    refactor: render_pdf.py to improve code structure and readability

--- render_pdf.py
+++ render_pdf.py
@@ -1,13 +1,11 @@
 from xhtml2pdf import pisa
 import optparse
 
-if __name__ == "__main__":
-    parser = optparse.OptionParser()
-    parser.add_option('-f', '--filename', dest='filename',
-                      help='HTML input filename')
-    parser.add_option('-o', '--output', dest='output', help='Output file path')
-    (options, args) = parser.parse_args()
+def write_html_to_pdf(html_content, output_file):
+    pisa_status = pisa.CreatePDF(html_content, dest=output_file)
+    return pisa_status.err
 
+def main(options, args):
     if not options.filename:
         print('HTML input filename is required')
         exit()
@@ -20,8 +18,17 @@ if __name__ == "__main__":
     html_content = html_file.read()
 
     with open(options.output, 'wb') as output_file:
-        pisa_status = pisa.CreatePDF(html_content, dest=output_file)
-        if pisa_status.err:
+        err = write_html_to_pdf(html_content, output_file)
+        if err:
             print('Error while rendering PDF')
         else:
             print('PDF successfully generated')
+
+
+if __name__ == "__main__":
+    parser = optparse.OptionParser()
+    parser.add_option('-f', '--filename', dest='filename',
+                      help='HTML input filename')
+    parser.add_option('-o', '--output', dest='output', help='Output file path')
+    (options, args) = parser.parse_args()
+    main(options, args)
